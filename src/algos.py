d1243d62d279286b8e42b558b19a41ce170cccd6
Theophile Cantelobre <theophilec@gmail.com>
2020-12-23 10:57:01 +0100

    Added OGD

--- src/algos.py
+++ src/algos.py
@@ -1,12 +1,15 @@
 from pathlib import Path
 from typing import List
-
+from tqdm import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import ticker
 
 from data_utils import load_processed_data
 from projection import l1_ball_proj
+
+
+np.random.seed(0)
 
 class Logger:
     def __init__(self, algo_tag: str):
@@ -164,13 +167,61 @@ def train_proj_gradient_descent(a: np.array, b: np.array, a_test: np.array, b_te
     return x, logger
 
 
-def main():
+def train_proj_online_gradient_descent(a: np.array, b: np.array, a_test: np.array, b_test: np.array, T: int, alpha: float, radius: float):
+    # add a column of ones to the input data, to avoid having to define an explicit bias in our weights
+    a = np.concatenate([a, np.ones((len(a), 1))], axis=1)
+    a_test = np.concatenate([a_test, np.ones((len(a_test), 1))], axis=1)
+    n, d = a.shape
+
+    # the weights of our SVM classifier
+    # x is the averaged weights (online to batch conversion)
+    x = np.zeros(d)
+    # y is weight (online version)
+    y = np.zeros(d)
+
+    logger = Logger(algo_tag=rf'P-OGD - $\alpha={alpha} - z={radius}$')
+    for t in tqdm(range(1, T + 1)):
+        # pick random sample
+        i = np.random.randint(n)
+        a_, b_ = a[i][np.newaxis, :], np.array([b[i]])
+
+        # log our results (before training, to match plots from the class)
+        logger.log(loss=hinge_loss(a, b, x, alpha),
+                   train_err=error(a, b, x),
+                   test_err=error(a_test, b_test, x))
+
+        if alpha == 0:
+            # our problem is simply convex (as the hinge loss is a convex function)
+            eta_t = 1 / np.sqrt(t)
+        else:
+            # thanks to the regularization, our problem is alpha strongly convex
+            # eta_t = 2 / (alpha * t)
+            eta_t = 1 / t
+
+        grad = hinge_loss_grad(a_, b_, y, alpha)
+
+        y = y - eta_t * grad
+        y, d_0, theta = l1_ball_proj(y, radius)
+        # TODO: we could log d_0 as well?
+
+        # averaging
+        x = (x * (t - 1) + y) / t
+
+
+    return x, logger
+
+
+def train_all():
     dir_data = Path(__file__).resolve().parents[1].joinpath('data/')
     x_train, y_train, x_test, y_test = load_processed_data(dir_data)
 
     # projected gradient descent
+    x, logger_p_ogd = train_proj_online_gradient_descent(
+        a=x_train, b=y_train, a_test=x_test, b_test=y_test, T=1000, alpha=0.33, radius=20.
+    )
+    # projected gradient descent
     x, logger_p_gd = train_proj_gradient_descent(
-        a=x_train, b=y_train, a_test=x_test, b_test=y_test, T=100, alpha=0.33, radius=10
+        a=x_train, b=y_train, a_test=x_test, b_test=y_test, T=100, alpha=0.33, radius=20.
     )
     # gradient descent
     x, logger_gd = train_gradient_descent(
@@ -178,8 +229,8 @@ def main():
     )
 
     # plot results
-    plot_results([logger_gd, logger_p_gd])
+    plot_results([logger_p_ogd, logger_gd, logger_p_gd])
 
 
 if __name__ == '__main__':
-    main()
+    train_all()
