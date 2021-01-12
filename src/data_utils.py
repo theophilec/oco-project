ccdb132243748b270f8b79099bfca627236f1685
Theophile Cantelobre <theophilec@gmail.com>
2021-01-12 17:22:49 +0100

    .

--- src/data_utils.py
+++ src/data_utils.py
@@ -16,18 +16,20 @@ def load_raw_data(dir_data: Path, print_descriptive_stats: bool):
         (x_train, y_train, x_test, y_test), the normalized (to (0, 1)) train/test MNIST data
 
     """
-    print('loading data...')
-    df_train = pd.read_csv(dir_data.joinpath('raw/mnist_train.csv'), header=None)
-    df_test = pd.read_csv(dir_data.joinpath('raw/mnist_test.csv'), header=None)
+    print("loading data...")
+    df_train = pd.read_csv(dir_data.joinpath("raw/mnist_train.csv"), header=None)
+    df_test = pd.read_csv(dir_data.joinpath("raw/mnist_test.csv"), header=None)
     x_train = df_train.values[:, 1:]
     y_train = df_train.values[:, 0]
     x_test = df_test.values[:, 1:]
     y_test = df_test.values[:, 0]
-    print(f'data loaded: train data {x_train.shape, y_train.shape}, test data {x_test.shape, y_test.shape}')
+    print(
+        f"data loaded: train data {x_train.shape, y_train.shape}, test data {x_test.shape, y_test.shape}"
+    )
 
     # normalize data
-    x_train = x_train / 255.
-    x_test = x_test / 255.
+    x_train = x_train / 255.0
+    x_test = x_test / 255.0
 
     # we are only interested in predicting the digit 0
     y_train = 2 * (y_train == 0) - 1
@@ -38,9 +40,11 @@ def load_raw_data(dir_data: Path, print_descriptive_stats: bool):
         _, counts_test = np.unique(y_test, return_counts=True)
         freq_0_train = counts_train[1] / counts_train.sum()
         freq_0_test = counts_test[1] / counts_test.sum()
-        print(f'Frequency of digit 0\n'
-              f'in train set : {counts_train[1]}/{counts_train.sum()} = {freq_0_train:.3f}\n'
-              f'in test set : {counts_test[1]}/{counts_test.sum()} = {freq_0_test:.3f}')
+        print(
+            f"Frequency of digit 0\n"
+            f"in train set : {counts_train[1]}/{counts_train.sum()} = {freq_0_train:.3f}\n"
+            f"in test set : {counts_test[1]}/{counts_test.sum()} = {freq_0_test:.3f}"
+        )
 
     return x_train, y_train, x_test, y_test
 
@@ -56,12 +60,12 @@ def load_processed_data(dir_data: Path):
         (x_train, y_train, x_test, y_test)
 
     """
-    print('loading processed data...')
-    x_train = np.load(str(dir_data.joinpath('processed/x_train.npy')))
-    y_train = np.load(str(dir_data.joinpath('processed/y_train.npy')))
-    x_test = np.load(str(dir_data.joinpath('processed/x_test.npy')))
-    y_test = np.load(str(dir_data.joinpath('processed/y_test.npy')))
-    print('done !')
+    print("loading processed data...")
+    x_train = np.load(str(dir_data.joinpath("processed/x_train.npy")))
+    y_train = np.load(str(dir_data.joinpath("processed/y_train.npy")))
+    x_test = np.load(str(dir_data.joinpath("processed/x_test.npy")))
+    y_test = np.load(str(dir_data.joinpath("processed/y_test.npy")))
+    print("done !")
     return x_train, y_train, x_test, y_test
 
 
@@ -80,17 +84,19 @@ def save_data(dir_data: Path, x_train, y_train, x_test, y_test):
     Returns:
 
     """
-    np.save(str(dir_data.joinpath('processed/x_train')), x_train)
-    np.save(str(dir_data.joinpath('processed/x_test')), x_test)
-    np.save(str(dir_data.joinpath('processed/y_train')), y_train)
-    np.save(str(dir_data.joinpath('processed/y_test')), y_test)
+    np.save(str(dir_data.joinpath("processed/x_train")), x_train)
+    np.save(str(dir_data.joinpath("processed/x_test")), x_test)
+    np.save(str(dir_data.joinpath("processed/y_train")), y_train)
+    np.save(str(dir_data.joinpath("processed/y_test")), y_test)
 
 
 def main():
-    dir_data = Path(__file__).resolve().parents[1].joinpath('data/')
-    x_train, y_train, x_test, y_test = load_raw_data(dir_data, print_descriptive_stats=True)
+    dir_data = Path(__file__).resolve().parents[1].joinpath("data/")
+    x_train, y_train, x_test, y_test = load_raw_data(
+        dir_data, print_descriptive_stats=True
+    )
     save_data(dir_data, x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
